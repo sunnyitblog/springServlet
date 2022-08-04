050ddcfba3c8b3bc24d403fd8a6266c4ab77fc4a
sunnyit <choiyk9118@naver.com>
2022-08-04 23:42:31 +0900

    first commit

---
+++ src/main/java/com/example/demo/hello/hello.java
@@ -0,0 +1,19 @@
+package com.example.demo.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="hello",urlPatterns = "/hello")
+public class hello extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("hello.service");
+    }
+}
+
