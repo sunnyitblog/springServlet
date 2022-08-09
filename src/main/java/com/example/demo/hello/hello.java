1821ce712ded25e78749abf6c16d212eabe2ec32
sunnyit <choiyk9118@naver.com>
2022-08-09 22:49:50 +0900

    파라미터 조회

--- src/main/java/com/example/demo/hello/hello.java
+++ src/main/java/com/example/demo/hello/hello.java
@@ -7,13 +7,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name="hello",urlPatterns = "/hello")
+@WebServlet(name="helloServlet",urlPatterns = "/hello")
 public class hello extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        System.out.println("hello.service");
+        System.out.println("helloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); //header 정보에 들어감
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello"+username);
     }
 }
 
